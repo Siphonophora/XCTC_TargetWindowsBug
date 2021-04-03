7de551efecd1c9ec8202c6d80d83fa22c70f65b7
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2021-04-03 13:52:31 -0400

    Add .Net 5 MS Test proj

---
+++ XCT_Bug/TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
